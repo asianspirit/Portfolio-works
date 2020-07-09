09d643156efa9a3d752456de121b86fd9e1670e6
asianspirit <63229901+asianspirit@users.noreply.github.com>
2020-07-09 17:14:45 +0600

    1

---
+++ work-js/main.js
@@ -0,0 +1,204 @@
+$(function () {
+
+
+
+    let cursor = {
+        delay: 8,
+        _x: 0,
+        _y: 0,
+        endX: (window.innerWidth / 2),
+        endY: (window.innerHeight / 2),
+        cursorVisible: true,
+        cursorEnlarged: false,
+        $dot: document.querySelector('.cursor-dot'),
+        $outline: document.querySelector('.cursor-dot-outline'),
+        
+        init: function() {
+            // Set up element sizes
+            this.dotSize = this.$dot.offsetWidth;
+            this.outlineSize = this.$outline.offsetWidth;
+            
+            this.setupEventListeners();
+            this.animateDotOutline();
+        },
+     
+        setupEventListeners: function() {
+            let self = this;
+            
+            // Anchor hovering
+            document.querySelectorAll('a').forEach(function(el) {
+                el.addEventListener('mouseover', function() {
+                    self.cursorEnlarged = true;
+                    self.toggleCursorSize();
+                });
+                el.addEventListener('mouseout', function() {
+                    self.cursorEnlarged = false;
+                    self.toggleCursorSize();
+                });
+            });
+            
+            // Click events
+            document.addEventListener('mousedown', function() {
+                self.cursorEnlarged = true;
+                self.toggleCursorSize();
+            });
+            document.addEventListener('mouseup', function() {
+                self.cursorEnlarged = false;
+                self.toggleCursorSize();
+            });
+      
+      
+            document.addEventListener('mousemove', function(e) {
+                // Show the cursor
+                self.cursorVisible = true;
+                self.toggleCursorVisibility();
+    
+                // Position the dot
+                self.endX = e.pageX;
+                self.endY = e.pageY;
+                self.$dot.style.top = self.endY + 'px';
+                self.$dot.style.left = self.endX + 'px';
+            });
+            
+            // Hide/show cursor
+            document.addEventListener('mouseenter', function(e) {
+                self.cursorVisible = true;
+                self.toggleCursorVisibility();
+                self.$dot.style.opacity = 1;
+                self.$outline.style.opacity = 1;
+            });
+            
+            document.addEventListener('mouseleave', function(e) {
+                self.cursorVisible = true;
+                self.toggleCursorVisibility();
+                self.$dot.style.opacity = 0;
+                self.$outline.style.opacity = 0;
+            });
+        },
+        
+        animateDotOutline: function() {
+            let self = this;
+            
+            self._x += (self.endX - self._x) / self.delay;
+            self._y += (self.endY - self._y) / self.delay;
+            self.$outline.style.top = self._y + 'px';
+            self.$outline.style.left = self._x + 'px';
+            
+            requestAnimationFrame(this.animateDotOutline.bind(self));
+        },
+        
+        toggleCursorSize: function() {
+            let self = this;
+            
+            if (self.cursorEnlarged) {
+                self.$dot.style.transform = 'translate(-50%, -50%) scale(0.75)';
+                self.$outline.style.transform = 'translate(-50%, -50%) scale(1.5)';
+            } else {
+                self.$dot.style.transform = 'translate(-50%, -50%) scale(1)';
+                self.$outline.style.transform = 'translate(-50%, -50%) scale(1)';
+            }
+        },
+        
+        toggleCursorVisibility: function() {
+            let self = this;
+            
+            if (self.cursorVisible) {
+                self.$dot.style.opacity = 1;
+                self.$outline.style.opacity = 1;
+            } else {
+                self.$dot.style.opacity = 0;
+                self.$outline.style.opacity = 0;
+            }
+        }
+    }
+    cursor.init();
+
+    $('.burger').on('click', function () {
+        $('.burger').toggleClass('open');
+        $('.burger-menu').toggleClass('open');
+        $('.home').toggleClass('open');
+        $('.works').toggleClass('open');
+        $('.about').toggleClass('open');
+       
+    });
+
+    $('#form .input').blur(function(){
+        if($(this).val().length > 0){
+          $(this).addClass('white');
+        } else {
+          $(this).removeClass('white');
+        }
+      });
+
+      let swiper = new Swiper('.swiper-container', {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      //   init: false,
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+          dynamicBullets: true,
+        },
+        breakpoints: {
+          440: {
+              slidesPerView: 1,
+              spaceBetween: 10,
+            },
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 4,
+            spaceBetween: 50,
+          },
+        }
+      });
+ 
+      const swup = new Swup();
+
+
+swup.on('contentReplaced', function() {
+    swup.options.containers.forEach((selector) => {
+        let swiper = new Swiper('.swiper-container', {
+            slidesPerView: 1,
+            spaceBetween: 10,
+          //   init: false,
+            pagination: {
+              el: '.swiper-pagination',
+              clickable: true,
+              dynamicBullets: true,
+            },
+            breakpoints: {
+              440: {
+                  slidesPerView: 1,
+                  spaceBetween: 10,
+                },
+              640: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+              },
+              768: {
+                slidesPerView: 4,
+                spaceBetween: 40,
+              },
+              1024: {
+                slidesPerView: 4,
+                spaceBetween: 50,
+              },
+            }
+          });
+
+    });
+  });
+
+      
+});
+
+
+
+
